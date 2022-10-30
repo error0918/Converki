743b0270f2586f6a2ba1ae0ca74958ef11ff78f8
error0918 <jtaeyeon05@gmail.com>
2022-10-31 01:06:23 +0900

    2022/10/31 01:06

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Converki
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Dictionary<string, Dictionary<string, float>> converter = new Dictionary<string, Dictionary<string, float>>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Dictionary<string, float> dimensionConverter = new Dictionary<string, float>();
+            dimensionConverter.Add("mm", 1);
+            dimensionConverter.Add("cm", 10);
+            dimensionConverter.Add("m", 10000);
+            dimensionConverter.Add("km", 10000000);
+            dimensionConverter.Add("inch(es)", 3.93701f);
+            converter.Add("넓이", dimensionConverter);
+
+            Dictionary<string, float> storageConverter = new Dictionary<string, float>();
+            dimensionConverter.Add("KB", 1);
+            dimensionConverter.Add("MB", 1024);
+            dimensionConverter.Add("GB", 1024 * 1024);
+            dimensionConverter.Add("TB", 1024 * 1024 * 1024);
+            converter.Add("용량", storageConverter);
+
+            Dictionary<string, float> timeConverter = new Dictionary<string, float>();
+            dimensionConverter.Add("초", 1);
+            dimensionConverter.Add("분", 60);
+            dimensionConverter.Add("시", 60 * 60);
+            dimensionConverter.Add("일", 60 * 60 * 24);
+            converter.Add("시간", timeConverter);
+
+            DataContext = this;
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
